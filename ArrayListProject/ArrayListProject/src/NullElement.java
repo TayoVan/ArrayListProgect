7b9239eafc4ebc0e0a14a781147aa6e03e501fb7
TayoVan <ksiunya.s@gmail.com>
2026-05-04 08:10:34 +0300

    Delete ArrayListProject directory

--- ArrayListProject/ArrayListProject/src/NullElement.java
+++
@@ -1,8 +0,0 @@
-public class NullElement extends RuntimeException{
-
-    public NullElement() {
-        super("Неможливо додати null-елемент до списку. " +
-                "Усі елементи мають бути ненульовими значеннями.");
-    }
-}
-
